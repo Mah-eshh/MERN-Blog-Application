b174ffef945d089b37ffb34cbafd01ef3c3acb00
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-07-06 14:26:03 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        <b>Mahesh Abeykoon</b>
+      </h1>
+      <h2>My blog Application</h2>
+    </div>
+  );
+};
+
+export default App;
