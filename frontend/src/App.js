559a0b8b940878a48f3d9eb8c371f562c9d8e76c
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-07-06 17:59:19 +0530

    Update file App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import { getPosts } from "./actions/posts";
+import useStyles from "./styles";
+import maheshblog from "./components/images/maheshblog";
 
 const App = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
   return (
-    <div>
-      <h1>
-        <b>Mahesh Abeykoon</b>
-      </h1>
-      <h2>My blog Application</h2>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Mahesh Blogs
+        </Typography>
+        <img
+          className={classes.image}
+          src={maheshblog}
+          alt="icon"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={13} sm={8}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={13} sm={5}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
