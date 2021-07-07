cfe2643516aeafe247141a25854babe09400a0d5
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-07-07 23:33:32 +0530

    Add searching Route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import {
   getPosts,
+  getPostsBySearch,
   getPost,
   createPost,
   updatePost,
@@ -10,12 +11,15 @@ import {
 } from "../controllers/posts.js";
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
+router.get("/search", getPostsBySearch);
 router.get("/", getPosts);
-router.post("/", createPost);
 router.get("/:id", getPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
