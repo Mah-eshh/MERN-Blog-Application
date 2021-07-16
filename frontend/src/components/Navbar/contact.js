bc82bd8db73222fcedfdb50aa64e51bd6a466364
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-07-16 20:54:53 +0530

    Implement and update connect.js file

--- frontend/src/components/Navbar/contact.js
+++ frontend/src/components/Navbar/contact.js
@@ -1,17 +1,46 @@
 import React from "react";
+import "./style.css";
 
 const Contact = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "90vh",
-        color: "red",
-      }}
-    >
-      <h1>This is contact page created by me.</h1>
+    <div class="container">
+      <div class="card">
+        <div class="content">
+          <h2>GitHub</h2>
+
+          <p>
+            Github is a web-based platform used for version control. Git
+            simplifies the process of working with other people and makes it
+            easy to collaborate on projects.
+          </p>
+          <a href="https://github.com/Mah-eshh"> Connect</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h2>Instagram</h2>
+
+          <p>
+            Instagram is a free, online photo-sharing application and social
+            network platform that was acquired by Facebook in 2012. So take a
+            look at my instagram account.
+          </p>
+          <a href="https://www.instagram.com/mahe_sh"> Connect</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h2>Facebook</h2>
+
+          <p>
+            Facebook is a social networking site that makes it easy for you to
+            connect with me.
+          </p>
+          <a href="https://www.facebook.com/Mahesh0Abeykoon"> Connect</a>
+        </div>
+      </div>
     </div>
   );
 };
