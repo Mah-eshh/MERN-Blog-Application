b7a119925c8466edac9cfbd1f210cddf71c25651
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-07-22 12:34:57 +0530

    Change look and feel 'about.js' file

--- frontend/src/components/Navbar/about.js
+++ frontend/src/components/Navbar/about.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "./style.css";
-import img from "./img.png";
+import imgg from "./imgg.png";
 
 const About = () => {
   return (
     <div class="aboutcontainer">
       <div class="aboutcard">
         <div class="imgBx">
-          <img src={img} />
+          <img src={imgg} />
           <div class="aboutcontent">
             <h3>About</h3>
+
             <p>
               Hey!... You are welcome to MAHESH blog App. This is a something
               new blog article web application. In here you can create a new
               account and you can also register with the App on google or other
               in your choice. But it is must to write a very new article of your
               own if you need. If you have any trouble or doubts feel free to
-              contact.
               <p>
-                <b>CONTACT</b>
+                <b>CONTACT ME @</b>
               </p>
               <a href="https://www.facebook.com/Mahesh0Abeykoon">
                 {" "}
                 <b>Facebook</b>
               </a>
-              <un></un>
+              <un> &</un>
               <a href="https://www.instagram.com/mahe_sh">
                 {" "}
                 <b>Instagram</b>
